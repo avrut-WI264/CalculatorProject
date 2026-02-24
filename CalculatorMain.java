25acaabc7f21b1ad448bc542c5bd0deee5e80f8b
avrut-WI264 <Avrut.Patel@wissen.com>
2026-02-24 16:31:39 +0530

    Initial commit

---
+++ CalculatorMain.java
@@ -0,0 +1,13 @@
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int sum = calc.add(10, 5);
+        int diff = calc.sub(10, 5);
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+    }
+}
